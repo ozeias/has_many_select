53e68625d5cc1a6042effb48a418480d571d214e
Ozéias Sant'ana <oz.santana@gmail.com>
2008-08-04 10:30:28 -0300

    Insert with primary_key

---
+++ lib/has_many_select/association_proxy.rb
@@ -0,0 +1,17 @@
+module ActiveRecord
+  module Associations
+    class AssociationProxy #:nodoc:
+      protected
+        def set_belongs_to_association_for(record)
+          if @reflection.options[:as]
+            record["#{@reflection.options[:as]}_id"]   = @owner.id unless @owner.new_record?
+            record["#{@reflection.options[:as]}_type"] = @owner.class.base_class.name.to_s
+          elsif @reflection.options[:primary_key]
+            record[@reflection.primary_key_name] = owner_quoted_id unless @owner.new_record?
+          else
+            record[@reflection.primary_key_name] = @owner.id unless @owner.new_record?
+          end
+        end
+    end
+  end
+end
